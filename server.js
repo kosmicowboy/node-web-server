83f28bb7c5da93945f0fde5059a40688cc143821
kgd <kylegraydev@gmail.com>
2017-05-14 00:19:31 -0700

    adds projects page

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
     });
 });
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'My project'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         errorMessage: 'Bad request'
